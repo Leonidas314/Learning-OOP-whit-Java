f787850fc77fd21d598da7eb5fa6345248b9962b
Leon <leondellafiore2016@gmail.com>
2024-10-02 21:06:38 -0300

    Add descriptive task file to exercise 1 , 3  and 4, setup initial java files to exercise 4

---
+++ Practice5_HashMaps_HashSets/Ejercicio4/Product.java
@@ -0,0 +1,58 @@
+/**
+ * Manage the stock in a business.
+ * The stock is described by zero or more Products.
+ * 
+ */
+public class StockManager
+{
+    /**
+     * @post A StockManager is created.
+     */
+    public StockManager()
+    {
+        //TODO complete the implementation
+    }
+
+    /**
+     * @post item are added to stock.
+     */
+    public void addProduct(Product item)
+    {
+        //TODO complete the implementation
+    }
+    
+    /**
+     * TODO complete the specification
+     */
+    public void delivery(int id, int amount)
+    {
+           //TODO complete the implementation 
+    }
+    
+    /**
+     * TODO complete the specification
+     */
+    public Product findProduct(int id)
+    {
+        //TODO complete the implementation 
+        return null;
+    }
+    
+    /**
+     * TODO complete the specification
+     */
+    public int numberInStock(int id)
+    {
+        //TODO complete the implementation 
+        return 0;
+    }
+
+    /**
+     * TODO complete the specification
+     */
+    public void printProductDetails()
+    {
+       //TODO complete the implementation 
+    }
+
+}
